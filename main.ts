2b29134d4380cc0513e6d69dc1153c18da22f4fd
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2021-11-06 20:28:03 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,66 @@
+makerbit.onIrButton(IrButton.Number_3, IrButtonAction.Pressed, function () {
+    soundExpression.spring.playUntilDone()
+})
+makerbit.onIrButton(IrButton.Number_8, IrButtonAction.Pressed, function () {
+    music.playMelody("B - - - - - - - ", 120)
+})
+makerbit.onIrButton(IrButton.Hash, IrButtonAction.Pressed, function () {
+    music.playMelody("E - - - - - - - ", 120)
+})
+makerbit.onIrButton(IrButton.Number_0, IrButtonAction.Pressed, function () {
+    music.playMelody("F - - - - - - - ", 120)
+})
+makerbit.onIrButton(IrButton.Number_1, IrButtonAction.Pressed, function () {
+    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+})
+makerbit.onIrButton(IrButton.Number_4, IrButtonAction.Pressed, function () {
+    music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once)
+})
+makerbit.onIrButton(IrButton.Number_5, IrButtonAction.Pressed, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+makerbit.onIrButton(IrButton.Number_6, IrButtonAction.Pressed, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
 makerbit.onIrButton(IrButton.Left, IrButtonAction.Pressed, function () {
     basic.showArrow(ArrowNames.West)
+})
+makerbit.onIrButton(IrButton.Number_7, IrButtonAction.Pressed, function () {
+    music.playMelody("C5 - - - - - - - ", 120)
 })
 makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
     basic.showArrow(ArrowNames.South)
 })
+makerbit.onIrButton(IrButton.Star, IrButtonAction.Pressed, function () {
+    music.playMelody("G - - - - - - - ", 120)
+})
 makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
     basic.showArrow(ArrowNames.North)
 })
-makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
-    serial.writeString("" + (makerbit.irButton()))
+makerbit.onIrButton(IrButton.Number_2, IrButtonAction.Pressed, function () {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
 })
 makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
-    basic.showArrow(ArrowNames.East)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
 })
 makerbit.onIrButton(IrButton.Ok, IrButtonAction.Pressed, function () {
     basic.clearScreen()
+    music.stopMelody(MelodyStopOptions.All)
+})
+makerbit.onIrButton(IrButton.Number_9, IrButtonAction.Pressed, function () {
+    music.playMelody("A - - - - - - - ", 120)
 })
 makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.Keyestudio)
 serial.writeString("Hello")
